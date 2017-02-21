31be1d8158eb0fa760ec2e993fd625f2aa173f6e
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-21 07:45:59 -0300

    Comparing a list of Craft Coder Guides by using an anonymous Java class and Collections.sort() method

--- java8-lambda-expression/src/test/java/com/craftcoder/java8/lambda/LambdaExpressionTest.java
+++ java8-lambda-expression/src/test/java/com/craftcoder/java8/lambda/LambdaExpressionTest.java
@@ -1,5 +1,13 @@
 package com.craftcoder.java8.lambda;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.junit.Test;
 
 public class LambdaExpressionTest {
@@ -22,6 +30,21 @@ public class LambdaExpressionTest {
 		Runnable runnable = () -> System.out.println("Awesome! Lambda Expression here!");
 
 		runnable.run();
+	}
+
+	@Test
+	public void shouldOrderTheListOfNamesByUsingAnonymousClass() throws Exception {
+		List<String> craftCoderGuides = Arrays.asList("Mockito", "CDI", "JUnit", "Hibernate", "Spring");
+
+		Collections.sort(craftCoderGuides, new Comparator<String>() {
+
+			@Override
+			public int compare(String firstGuide, String secondGuide) {
+				return firstGuide.compareTo(secondGuide);
+			}
+		});
+
+		assertThat(craftCoderGuides, contains("CDI", "Hibernate", "JUnit", "Mockito", "Spring"));
 	}
 
 }
