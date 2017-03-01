fb8e1bc75faec6e350be521db2ea171dc184eebe
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-03-01 15:13:01 -0300

    Initial commit with the structure to explain about Java 8 Supplier Functional Interface

---
+++ java8-suppliers/src/test/java/com/craftcoder/java8/suppliers/SupplierFunctionalInterfaceTest.java
@@ -0,0 +1,12 @@
+package com.craftcoder.java8.suppliers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+
+public class SupplierFunctionalInterfaceTest {
+
+}
